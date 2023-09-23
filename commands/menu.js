d6c23b2ff7c3ec641d605dbbb94fbb42a236aa56
AbhiDev <114732679+AbhiDevOfficial@users.noreply.github.com>
2023-09-23 16:53:25 +0530

    Add files via upload

--- commands/menu.js
+++ commands/menu.js
@@ -1,1 +1,14 @@
+const { allCommands } = require('../index');
+const config = require('../config');
 
+module.exports = {
+  command: 'menu',
+  info: '',
+  func: async (sock, msg, text) => {
+    let cmd = '';
+    allCommands().forEach(async (cm) => {
+       if (cm.command !== 'menu') cmd += '*' + config.PREFIX + cm.command + '*\n```' + cm.info + '```\n\n';
+    });
+    return await msg.reply(cmd);
+  }
+}
