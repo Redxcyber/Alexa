1cb1b4332f5b2e51de6d51519067f9ba03180683
AbhiDev <114732679+AbhiDevOfficial@users.noreply.github.com>
2023-09-23 16:55:20 +0530

    Add files via upload

--- helpers/utils.js
+++ helpers/utils.js
@@ -1,1 +1,21 @@
+const axios = require('axios');
 
+async function parseJson(url) {
+  try {
+   let { data } = await axios.get(url);
+   return data;
+  } catch (e) {
+   console.log('An error occurred while fetching JSON:\n\n' + e.stack);
+   return false;
+  }
+}
+
+function numericalToString(number) {
+  if (isNaN(number)) return false;
+  if (number >= 1000000) return (number / 1000000).toFixed(1) + 'M';
+  else if (number >= 1000) return (number / 1000).toFixed(1) + 'K';
+  else return number.toString();
+}
+
+
+module.exports = { parseJson, numericalToString };
