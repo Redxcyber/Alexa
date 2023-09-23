9cf8e5342a5652618732b0f62ce929d89894b8b4
AbhiDev <114732679+AbhiDevOfficial@users.noreply.github.com>
2023-09-23 16:49:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,59 +4,77 @@ const colors = require('colors');
 const qrcode = require('qrcode-terminal');
 const fs = require('fs');
 
-let commands = [];
-
 async function Connect() {
-    let store = makeInMemoryStore({
-       logger: pino().child({ level: 'silent', stream: 'store' })
-    })
-    let { version, isLatest } = await fetchLatestBaileysVersion();
-    let { state, saveCreds } = await useMultiFileAuthState('./session')
-    let sock = makeWASocket({ logger: pino({ level: 'silent' }), printQRInTerminal: true, markOnlineOnConnect: false, browser: ['Darky','Chrome','1.0.0'], auth: state, version: version })
-    store.bind(sock.ev)
+    try {
+        let store = makeInMemoryStore({
+            logger: pino().child({ level: 'silent', stream: 'store' })
+        });
 
-    fs.readdirSync('./commands').forEach(file => {
-     if (file.endsWith('.js')) {
-      let command = require('./commands/' + file);
-      commands.push({ command: command.command, info: command.info, func: command.func });
-     }
-    });
-    
-    sock.ev.on('connection.update', async (update) => {
-        const { connection } = update
-        if (connection === 'close') {
-           console.log(colors.red('Connection Closed: Reconnecting...'));
-           return await Connect();
-        }
-        else if (connection == 'open') {
-          console.log(colors.green('Connected!'));
-        }
-    })
-  
-    sock.ev.on('messages.upsert', async (msg) => {
-       msg = msg.messages[0]
-       if (!msg.message) return;
-       if (msg.key && msg.key.remoteJid === 'status@broadcast') return;
-       await require('./message')(msg, sock);
+        let { version, isLatest } = await fetchLatestBaileysVersion();
+        let { state, saveCreds } = await useMultiFileAuthState('./session');
+        let sock = makeWASocket({
+            logger: pino({ level: 'silent' }),
+            printQRInTerminal: true,
+            markOnlineOnConnect: false,
+            browser: ['Darky', 'Chrome', '1.0.0'],
+            auth: state,
+            version: version
+        });
+        store.bind(sock.ev);
 
-       if (msg.text.startsWith('>') && msg.key.fromMe) {
-        var evaluate = false
-        try {
-         evaluate = await eval(msg.text.replace('> ', '').toString());
-         try { evaluate = JSON.stringify(evaluate, null, 2) } catch {}
-        } catch (e) {
-         evaluate = e.stack.toString()
-        }
-        await msg.reply(evaluate);
-       }
-     
-       commands.forEach(async (command) => {
-        if (msg.text.startsWith('/'+command.command) && msg.key.fromMe) return command.func(sock, msg);
-       });
-    })
+        sock.ev.on('connection.update', async (update) => {
+            const { connection } = update;
+            if (connection === 'close') {
+                console.log(colors.red('Connection Closed: Reconnecting...'));
+                await Connect();
+            } else if (connection === 'open') {
+                console.log(colors.green('Connected!'));
+            }
+        });
 
-    sock.ev.on('creds.update', saveCreds)
+        sock.ev.on('messages.upsert', async (msg) => {
+            msg = msg.messages[0];
+            if (!msg.message) return;
+            if (msg.key && msg.key.remoteJid === 'status@broadcast') return;
+            console.log(msg);
+            msg = await require('./message')(msg, sock);
+
+            if (msg.text.startsWith('>') && msg.key.fromMe) {
+                var evaluate = false;
+                try {
+                    evaluate = await eval(msg.text.replace('> ', '').toString());
+                    try { evaluate = JSON.stringify(evaluate, null, 2); } catch {}
+                } catch (e) {
+                    evaluate = e.stack.toString();
+                }
+                await msg.reply(evaluate);
+            }
+
+            allCommands().forEach(async (command) => {
+                if (msg.key.fromMe) {
+                    let text = (msg.text.split(command.command)[1])?.trim();
+                    if (msg.text.startsWith('/' + command.command)) return command.func(sock, msg, text);
+                }
+            });
+        });
+
+        sock.ev.on('creds.update', saveCreds);
+    } catch (e) {
+        console.log(e);
+    }
 }
+
+function allCommands() {
+ let commands = [];
+ fs.readdirSync('./commands').forEach(file => {
+  if (file.endsWith('.js')) {
+   let command = require('./commands/' + file);
+   commands.push({ command: command.command, info: command.info, func: command.func });
+  }
+ });
+ return commands;
+}
+
 Connect();
 
-module.exports = { Connect, commands };
+module.exports = { Connect, allCommands };
