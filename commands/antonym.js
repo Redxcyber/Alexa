10f4d04f575f4d71c87d1096e254cd9f86775aac
AbhiDev <114732679+AbhiDevOfficial@users.noreply.github.com>
2023-09-28 13:47:02 +0530

    Create antonym.js

---
+++ commands/antonym.js
@@ -0,0 +1,17 @@
+const { parseJson } = require('../helpers/utils');
+
+module.exports = {
+  command: 'antonym',
+  info: 'Searches antonyms for given term.',
+  func: async (sock, msg, text) => {
+    if (!text) return await sock.editMessage(msg.key, '*Please enter a term to find antonyms!*');
+    let json = await parseJson('https://tuna.thesaurus.com/pageData/' + text);
+    let data = json.data.definitionData.definitions[0].antonym;
+    if (data.length < 1) return await sock.editMessage(msg.key, '*Unable to find antonyms for ' + text + '!*');
+    let res = '*Antonyms for:* _' + text + '_\n\n';
+    data.forEach((d, i) => {
+     if (i < 11) res += '*' + (i + 1) + '. ' + d.term + '*\n';
+    });
+    return await sock.editMessage(msg.key, res);
+  }
+};
