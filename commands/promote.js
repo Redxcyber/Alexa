97ade5ee2a030c3f333a09be32f5526ebe7a061f
AbhiDev <114732679+AbhiDevOfficial@users.noreply.github.com>
2023-09-28 16:10:31 +0530

    Create promote.js

---
+++ commands/promote.js
@@ -0,0 +1,16 @@
+module.exports = {
+  command: 'promote',
+  info: 'Gives admin authority to replied or mentioned user.',
+  func: async (sock, msg, text) => {
+    if (!text && !msg.replied) return await sock.editMessage(msg.key, '*Please reply or mention any user!*');
+    if (msg.replied) {
+      await sock.groupParticipantsUpdate(msg.chat, [msg.replied.sender], 'promote');
+      return await sock.editMessage(msg.key, '*Successfully promoted @' + msg.replied.sender.split('@')[0] + '!*');
+    } else if (msg.mentions) {
+      msg.mentions.map(async (user) => {
+        await sock.groupParticipantsUpdate(msg.chat, [user], 'promote');
+      });
+      return await sock.editMessage(msg.key, '*Successfully promote:*\n@' + msg.mentions.map((user) => user.split('@')[0]).join('\n@'));
+    }
+  }
+};
