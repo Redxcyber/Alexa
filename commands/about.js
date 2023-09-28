a6dc2785c55a0f12857ac2be4dce66d711958c50
AbhiDev <114732679+AbhiDevOfficial@users.noreply.github.com>
2023-09-28 14:25:07 +0530

    Create about.js

---
+++ commands/about.js
@@ -0,0 +1,29 @@
+module.exports = {
+  name: 'about',
+  info: 'Gets about of replied user or sets about with provided text.',
+  func: async (sock, msg, text) => {
+    if (!text && !msg.replied && msg.mention.length < 1) return await sock.editMessage(msg.key, '*Please reply or mention any user to get about or enter some text to set about!*');
+    if (msg.replied) {
+      return await sock.editMessage(
+        msg.key,
+        await sock.fetchStatus(msg.replied.sender)
+         .about
+      );
+    } else if (msg.mention.length > 0) {
+      msg.mention.map((user) => {
+        return await sock.editMessage(
+          msg.key,
+          '*' + await sock.getName(user) + ':* '
+          + await sock.fetchStatus(user)
+           .about
+          );
+      });
+    } else if (text) {
+      await sock.setAbout(msg.me, text).then(async (_) => {
+       return await sock.editMessage(msg.key, '*Successfully set about!*');
+      }).catch((_) => {
+       return await sock.editMessage(msg.key, '*Unable to set about!*');
+      });
+    }
+  }
+};
