c39f6eab01aab57e9985fb7edbbc50d1e3134fa9
AbhiDev <114732679+AbhiDevOfficial@users.noreply.github.com>
2023-09-28 15:56:06 +0530

    Update add.js

--- commands/add.js
+++ commands/add.js
@@ -7,7 +7,7 @@ module.exports = {
       await sock.groupParticipantsUpdate(msg.chat, [msg.replied.sender], 'add');
       return await sock.editMessage(msg.key, '*Successfully added!*');
     } else if (text) {
-      if (!text.startsWith('+')) return await sock.editMessage(msg.key, '*Please enter a number with +country code!*\n\n*For Example:*\n*/add +91 xxxxx xxxxx*\n\n*/add +91 xxxxxxxxxx\n*/add +91xxxxxxxxxx*');
+      if (!text.startsWith('+')) return await sock.editMessage(msg.key, '*Please enter a number with +country code!*\n\n*For Example:*\n*/add +91 xxxxx xxxxx*\n*/add +91 xxxxxxxxxx*\n*/add +91xxxxxxxxxx*');
       text = (text.trim().replace('+', '')) + '@s.whatsapp.net';
       await sock.groupParticipantsUpdate(msg.chat, [text], 'add');
       return await sock.editMessage(msg.key, '*Successfully added!*');
