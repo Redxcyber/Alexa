28e6205fae7f33578118f303c439ba487e97a7a2
AbhiDev <114732679+AbhiDevOfficial@users.noreply.github.com>
2023-09-28 13:44:18 +0530

    Update synonym.js

--- commands/synonym.js
+++ commands/synonym.js
@@ -2,7 +2,7 @@ const { parseJson } = require('../helpers/utils');
 
 module.exports = {
   command: 'synonym',
-  info: 'Finds synonyms for given term.',
+  info: 'Searches synonyms for given term.',
   func: async (sock, msg, text) => {
     if (!text) return await sock.editMessage(msg.key, '*Please enter a term to find synonyms!*');
     let json = await parseJson('https://tuna.thesaurus.com/pageData/' + text);
