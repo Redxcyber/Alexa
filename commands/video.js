6176643abefd7a480258c50f8f025ebfc97c63d9
AbhiDev <114732679+AbhiDevOfficial@users.noreply.github.com>
2023-10-23 13:14:34 +0530

    Update video.js

--- commands/video.js
+++ commands/video.js
@@ -5,20 +5,21 @@ module.exports = {
   info: 'Downloads video from YouTube from its URL.',
   func: async (sock, msg, text) => {
     let video = '';
-    let dmsg = await msg.reply('*Downloading video*')
     try {
       video = text.replace(/watch?v=/g, '').split('/')[3];
     } catch {
       return await sock.editMessage(dmsg.key, '*❌ Invalid url, Please enter a valid youtube video url.*');
     }
-    await sock.editMessage(dmsg.key, '*Downloading video.*');
-    let vid = ytdl(video, {filter: format => format.container === 'mp4' && ['720p', '480p', '360p', '240p', '144p'].map(() => true)});
-    await sock.editMessage(dmsg.key, '*Downloading video..*');
-    vid.pipe(fs.createWriteStream('../' + video + '.mp4'));
-    await sock.editMessage(dmsg.key, '*Downloading video...*');
+    await sock.editMessage(msg.key, '*Downloading video...*');
+    let file = __dirname + '/src/' + video + '.mp4';
+    let vid = ytdl(video, {
+      filter: format => format.container === 'mp4' && ['720p', '480p', '360p', '240p', '144p']
+        .map(() => true)
+    });
+    vid.pipe(fs.createWriteStream(file));
     vid.on('end', async () => {
-      await sock.sendMessage(msg.chat, { delete: dmsg.key })
-      await sock.sendMessage(msg.chat, { video: fs.readFileSync('../' + video + '.mp4'), mimetype: 'video/mpeg' });
+      await sock.sendMessage(msg.chat, { delete: msg.key })
+      await sock.sendMessage(msg.chat, { video: fs.readFileSync(file), mimetype: 'video/mpeg' });
     });
   }
 };
