55478ea064cc1782f2168760c55b264c52e5d6bf
AbhiDev <114732679+AbhiDevOfficial@users.noreply.github.com>
2023-10-23 13:20:18 +0530

    Update video.js

--- commands/video.js
+++ commands/video.js
@@ -11,7 +11,7 @@ module.exports = {
       return await sock.editMessage(dmsg.key, '*❌ Invalid url, Please enter a valid youtube video url.*');
     }
     await sock.editMessage(msg.key, '*Downloading video...*');
-    let file = __dirname + '/src/' + video + '.mp4';
+    let file = '../src/' + video + '.mp4';
     let vid = ytdl(video, {
       filter: format => format.container === 'mp4' && ['720p', '480p', '360p', '240p', '144p']
         .map(() => true)
