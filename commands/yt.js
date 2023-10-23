b8a4d74cea0a4584a8a7005046fcb8aac20d89c4
AbhiDev <114732679+AbhiDevOfficial@users.noreply.github.com>
2023-10-23 14:32:19 +0530

    Create yt.js

---
+++ commands/yt.js
@@ -0,0 +1,16 @@
+const yt = require('yt-search');
+
+module.exports = {
+  command: 'yts',
+  info: 'Searches video in YouTube for give query.',
+  func: async (sock, msg, text) => {
+   if (!text) return await sock.editMessage(msg.key, '*Please enter your query to search in YouTube!*');
+   await sock.editMessage(msg.key, '*🔍 Searching...*');
+   try {
+    let results = await yts(match[1]);
+   } catch {
+    return await sock.editMessage(msg.key, '*❌ Unable to find results for your query!*');
+   }
+   await sock.editMessage(msg.key, results.all.map((v) => '*' + v.title + '*\n' + v.url).join('\n\n'));
+  }
+};
