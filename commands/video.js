bff33668a93f9d302ededd0be39d3760c9020636
AbhiDev <114732679+AbhiDevOfficial@users.noreply.github.com>
2023-10-23 13:44:54 +0530

    Update video.js

--- commands/video.js
+++ commands/video.js
@@ -8,14 +8,15 @@ module.exports = {
     try {
       video = text.replace(/watch?v=/g, '').split('/')[3];
     } catch {
-      return await sock.editMessage(dmsg.key, '*❌ Invalid url, Please enter a valid youtube video url.*');
+      await sock.editMessage(dmsg.key, '*❌ Invalid url, Please enter a valid youtube video url.*');
     }
     await sock.editMessage(msg.key, '*Downloading video...*');
     let file = video + '.mp4';
     let vid = ytdl(video, {
       filter: format => format.container === 'mp4' && ['720p', '480p', '360p', '240p', '144p']
         .map(() => true)
-    }).pipe(fs.createWriteStream(file));
+    })
+    vid.pipe(fs.createWriteStream(file));
     vid.on('end', async () => {
       await sock.sendMessage(msg.chat, { delete: msg.key })
       await sock.sendMessage(msg.chat, { video: fs.readFileSync(file), mimetype: 'video/mpeg' });
