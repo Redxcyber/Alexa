ac330a437c25a808ee4d23e9df70d5e6a613d319
AbhiDev <114732679+AbhiDevOfficial@users.noreply.github.com>
2023-10-24 15:47:47 +0530

    Update antonym.js

--- commands/antonym.js
+++ commands/antonym.js
@@ -4,14 +4,14 @@ module.exports = {
   command: 'antonym',
   info: 'Searches antonyms for given term.',
   func: async (sock, msg, text) => {
-    if (!text) return await sock.editMessage(msg.key, '*Please enter a term to find antonyms!*');
+    if (!text) return await msg.reply('*Please enter a term to find antonyms!*');
     let json = await parseJson('https://tuna.thesaurus.com/pageData/' + text);
     let data = json.data.definitionData.definitions[0].antonyms;
-    if (data.length < 1) return await sock.editMessage(msg.key, '*Unable to find antonyms for ' + text + '!*');
+    if (data.length < 1) return await msg.reply('*Unable to find antonyms for ' + text + '!*');
     let res = '*Antonyms for:* _' + text + '_\n\n';
     data.forEach((d, i) => {
-     if (i < 11) res += '*' + (i + 1) + '. ' + d.term + '*\n';
+     if (i <= 10) res += '*' + (i + 1) + '. ' + d.term + '*\n';
     });
-    return await sock.editMessage(msg.key, res);
+    return await msg.reply(res);
   }
 };
