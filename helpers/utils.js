d0defc03c2841426c61f0653639f0807deb72825
AbhiDev <114732679+AbhiDevOfficial@users.noreply.github.com>
2023-10-24 16:08:00 +0530

    Update utils.js

--- helpers/utils.js
+++ helpers/utils.js
@@ -11,9 +11,7 @@ async function parseJson(url) {
 }
 
 function isUrl(url) {
-  return url.match(
-    /https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/
-  );
+  return (/^(https?:\/\/)?(www\.)?([a-zA-Z0-9-]+\.){1,}[a-zA-Z]{2,}/).test(url);
 }
 
 function numericalToString(number) {
