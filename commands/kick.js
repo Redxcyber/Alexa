04c16f22adfd1841ceab6f7daf0fb5d852654730
AbhiDev <114732679+AbhiDevOfficial@users.noreply.github.com>
2023-10-24 15:52:05 +0530

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -2,15 +2,15 @@ module.exports = {
   command: 'kick',
   info: 'Removes replied or mentioned user from group.',
   func: async (sock, msg, text) => {
-    if (!text && !msg.replied) return await sock.editMessage(msg.key, '*Please reply or mention any user!*');
+    if (!text && !msg.replied) return await msg.reply('*Please reply or mention any user!*');
     if (msg.replied) {
       await sock.groupParticipantsUpdate(msg.chat, [msg.replied.sender], 'remove');
-      return await sock.editMessage(msg.key, '*Successfully removed @' + msg.replied.sender.split('@')[0] + '!*');
+      return await msg.reply('*Successfully removed @' + msg.replied.sender.split('@')[0] + '!*');
     } else if (msg.mentions) {
       msg.mentions.map(async (user) => {
         await sock.groupParticipantsUpdate(msg.chat, [user], 'remove');
       });
-      return await sock.editMessage(msg.key, '*Successfully removed:*\n@' + msg.mentions.map((user) => user.split('@')[0]).join('\n@'));
+      return await msg.reply('*Successfully removed:*\n@' + msg.mentions.map((user) => user.split('@')[0]).join('\n@'));
     }
   }
 };
