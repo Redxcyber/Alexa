124985b3a7b9ac08e108f6be24d0399a0d9f2ff0
AbhiDev <114732679+AbhiDevOfficial@users.noreply.github.com>
2023-10-24 11:05:46 +0530

    Update song.js

--- commands/song.js
+++ commands/song.js
@@ -7,6 +7,7 @@ module.exports = {
   func: async (sock, msg, text) => {
     if (!text) return await sock.editMessage(msg.key, '*Please enter a song lyric!*');
     text += text.includes('http') ? '' : ' song';
+    await sock.editMessage('*🔍 Searching for song...*');
     let res = '';
     try {
      res = (await yt(text).all[0].url).split('/').slice(-1)[0];
@@ -14,14 +15,19 @@ module.exports = {
      return await sock.editMessage(msg.key, '*❌ Unable to find any song in this lyric!*');
     }
     let file = './' + res + '.mp3'
+    await sock.editMessage('*⬇️ Downloading song...*');
     try {
      let audio = ytdl(res, {
-      filter: format => format.container === 'mp3' && []
-        .map(() => true)
-     })
+      filter: 'audioonly',
+      quality: 'highestaudio'
+     });
      audio.pipe(fs.createWriteStream(file));
      audio.on('end', async () => {
-       return await sock.sendMessage(msg.chat, { audio: fs.readFileSync(file), mimetype: 'audio/mp3', ptt: false });
+       await sock.sendMessage(msg.chat,
+         { delete: msg.key }
+       ).then(async () => {
+         await sock.sendMessage(msg.chat, { audio: fs.readFileSync(file), mimetype: 'audio/mp3', ptt: false });
+       });
      });
    } catch (e) {
      await msg.reply(String(e));
