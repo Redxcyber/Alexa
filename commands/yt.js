a5138c25d6abbdfe6d6743031d5b21804b86174d
AbhiDev <114732679+AbhiDevOfficial@users.noreply.github.com>
2023-10-24 09:23:42 +0530

    Update yt.js

--- commands/yt.js
+++ commands/yt.js
@@ -8,7 +8,7 @@ module.exports = {
    await sock.editMessage(msg.key, '*🔍 Searching...*');
    try {
     let results = await yt(text);
-    await sock.editMessage(msg.key, results.all.map((v) => '_Title_ : *' + v.title + '*\n_URL_ : ' + v.url + '\n_Duration_ : *' + v.timestamp + '*\n_Viewd_ : *' + v.views + '*\n_Uploaded_ : *' + v.ago + '*\n_Uploaded by_ : *' + v.author.name + '*\n_Channel_ : ' + v.author.url).join('\n\n'));
+    await sock.editMessage(msg.key, results.all.map((v) => '_Title_ : *' + v.title + '*\n_URL_ : ' + (v.url.replace('watch?v=', '').replace('youtube.com', 'youtu.be')) + '\n_Duration_ : *' + v.timestamp + '*\n_Views_ : *' + v.views + '*\n_Uploaded_ : *' + v.ago + '*\n_Uploaded by_ : *' + v.author.name + '*\n_Channel_ : ' + v.author.url).join('\n\n'));
    } catch {
     return await sock.editMessage(msg.key, '*❌ Unable to find results for your query!*');
    }
