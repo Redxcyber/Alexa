10df678791579f5a890f4b0cb2493eedada30db5
AbhiDev <114732679+AbhiDevOfficial@users.noreply.github.com>
2023-10-24 15:33:11 +0530

    Update utils.js

--- helpers/utils.js
+++ helpers/utils.js
@@ -10,6 +10,12 @@ async function parseJson(url) {
   }
 }
 
+function isUrl(url) {
+  return url.match(
+    /https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/
+  );
+}
+
 function numericalToString(number) {
   if (isNaN(number)) return false;
   if (number >= 1000000) return (number / 1000000).toFixed(1) + 'M';
@@ -18,4 +24,4 @@ function numericalToString(number) {
 }
 
 
-module.exports = { parseJson, numericalToString };
+module.exports = { parseJson, isURL, numericalToString };
