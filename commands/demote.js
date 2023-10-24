38525fae3b2160c165897eac3eea7804b9323d10
AbhiDev <114732679+AbhiDevOfficial@users.noreply.github.com>
2023-10-24 15:49:11 +0530

    Update demote.js

--- commands/demote.js
+++ commands/demote.js
@@ -2,15 +2,15 @@ module.exports = {
   command: 'demote',
   info: 'Takes admin authority back from replied or mentioned user.',
   func: async (sock, msg, text) => {
-    if (!text && !msg.replied) return await sock.editMessage(msg.key, '*Please reply or mention any user!*');
+    if (!text && !msg.replied) return await msg.reply('*Please reply or mention any user!*');
     if (msg.replied) {
       await sock.groupParticipantsUpdate(msg.chat, [msg.replied.sender], 'demote');
-      return await sock.editMessage(msg.key, '*Successfully demoted @' + msg.replied.sender.split('@')[0] + '!*');
+      return await msg.reply('*Successfully demoted @' + msg.replied.sender.split('@')[0] + '!*');
     } else if (msg.mentions) {
       msg.mentions.map(async (user) => {
         await sock.groupParticipantsUpdate(msg.chat, [user], 'demote');
       });
-      return await sock.editMessage(msg.key, '*Successfully demoted:*\n@' + msg.mentions.map((user) => user.split('@')[0]).join('\n@'));
+      return await msg.reply('*Successfully demoted:*\n@' + msg.mentions.map((user) => user.split('@')[0]).join('\n@'));
     }
   }
 };
