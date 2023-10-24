8bca130f9a760da29d12ad7b899b2b33bd58da37
AbhiDev <114732679+AbhiDevOfficial@users.noreply.github.com>
2023-10-24 14:21:41 +0530

    Update ig.js

--- commands/ig.js
+++ commands/ig.js
@@ -5,8 +5,16 @@ module.exports = {
   info: 'Downloads instagram post/reels/stories from given url.',
   func: async (sock, msg, text) => {
     if (!text) return await sock.editMessage(msg.key, '*Please enter instagram post, reels or story url!*');
-    await instagram(text).then(async (result) => {
+    await instagram(text)
+     .then(async (result) => {
       if (!result.status) return await sock.editMessage(msg.key, '*❌ Invalid url, Please enter a valid instagram post/reels/story url!*');
-    });
+      for (let ig of result.data) {
+        if (ig.type == 'image') {
+          return await sock.sendMessage(msg.chat, { image: ig.url, mimetype: 'image/png', thumbnail: Buffer.alloc(0) });
+        } else {
+          return await sock.sendMessage(msg.chat, { video: ig.url, mimetype: 'video/mp4', thumbnail: Buffer.alloc(0) });
+        }
+      }
+    })
   }
 };
