0be5699f2cb32ce4bff850caa2747ae1f3f88508
AbhiDev <114732679+AbhiDevOfficial@users.noreply.github.com>
2023-10-24 15:53:15 +0530

    Update promote.js

--- commands/promote.js
+++ commands/promote.js
@@ -2,15 +2,15 @@ module.exports = {
   command: 'promote',
   info: 'Gives admin authority to replied or mentioned user.',
   func: async (sock, msg, text) => {
-    if (!text && !msg.replied) return await sock.editMessage(msg.key, '*Please reply or mention any user!*');
+    if (!text && !msg.replied) return await msg.reply('*Please reply or mention any user!*');
     if (msg.replied) {
       await sock.groupParticipantsUpdate(msg.chat, [msg.replied.sender], 'promote');
-      return await sock.editMessage(msg.key, '*Successfully promoted @' + msg.replied.sender.split('@')[0] + '!*');
+      return await msg.reply('*Successfully promoted @' + msg.replied.sender.split('@')[0] + '!*');
     } else if (msg.mentions) {
       msg.mentions.map(async (user) => {
         await sock.groupParticipantsUpdate(msg.chat, [user], 'promote');
       });
-      return await sock.editMessage(msg.key, '*Successfully promoted:*\n@' + msg.mentions.map((user) => user.split('@')[0]).join('\n@'));
+      return await msg.reply('*Successfully promoted:*\n@' + msg.mentions.map((user) => user.split('@')[0]).join('\n@'));
     }
   }
 };
