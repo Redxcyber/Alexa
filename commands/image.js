1edc056507d9b49b27218b96b9c2ec4cdbeb1a4b
AbhiDev <114732679+AbhiDevOfficial@users.noreply.github.com>
2023-10-24 14:53:44 +0530

    Create image.js

---
+++ commands/image.js
@@ -0,0 +1,25 @@
+const g_i_s = require('g-i-s');
+
+module.exports = {
+  command: 'image',
+  info: 'Searches image in google for given query.',
+  func: async (sock, msg, text) => {
+    if (!text) return await sock.editMessage(msg.key, '*Please enter your query to search for image in google!*');
+    if (/(porn|xxx|x-rated|adult|sex|nude|erotic|playboy|penthouse|strip|pornographic|liplock|🍑|🍌|🍆|buttock|vagina|dick|penis|pussy)/.test(text)) return await sock.editMessage(msg.key, '*⚠️ This query is banned!*');
+    let cl;
+    if (cl = text.match('\\{([0-9]{1})\\}')) {
+     cl = cl[1]
+    } else {
+     cl = 5
+    }
+    await sock.editMessage(msg.key, '*⬇️ Downloading {} images...*'.replace('{}', cl));
+    await g_i_s(text, async (error, result) => {
+     let count = []
+     count.length = cl
+     for (let c of count) {
+      let url = result[Math.floor(Math.random() * result.length)].url
+      await client.sendMessage(msg.chat, { image: { url: url }, mimetype: 'image/png' });
+     }
+    });
+   }
+};
