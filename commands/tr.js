6468fec8ea067be8f250775c728ccbdc9a6548e1
AbhiDev <114732679+AbhiDevOfficial@users.noreply.github.com>
2023-10-24 15:56:30 +0530

    Update tr.js

--- commands/tr.js
+++ commands/tr.js
@@ -4,10 +4,10 @@ module.exports = {
   command: 'tr',
   info: 'Translates replied message from auto-dected language to provided language.',
   func: async (sock, msg, text) => {
-    if (!text) return await sock.editMessage(msg.key, '*Please enter any text!*');
+    if (!text) return await msg.reply('*Please enter any text!*');
     let lang = text.match('\\{([a-zA-Z]{2})\\}') ? text.match('\\{([a-zA-Z]{2})\\}')[1] : false;
-    if (!lang) return sock.editMessage(msg.key, '*Please enter or reply to any text message along with the language code inside curly brace ( {} )!*\n\n*For example:*\n*- /tr I am Darky {hi}*\n*Above example translates the text \'I am Darky\' to hindi language.*\n\n*- /tr Hello, How are you? {es}*\n*Above example translates the text \'Hello, How are you?\' to spanish language.\n\n*Don\'t know 2-digit language code of your language?*\n*Checkout:* https://en.m.wikipedia.org/wiki/List_of_ISO_639-1_codes');
+    if (!lang) return msg.reply('*Please enter or reply to any text message along with the language code inside curly brace ( {} )!*\n\n*For example:*\n*- /tr I am Darky {hi}*\n*Above example translates the text \'I am Darky\' to hindi language.*\n\n*- /tr Hello, How are you? {es}*\n*Above example translates the text \'Hello, How are you?\' to spanish language.\n\n*Don\'t know 2-digit language code of your language?*\n*Checkout:* https://en.m.wikipedia.org/wiki/List_of_ISO_639-1_codes');
     let result = await translate(text.replace(/\{([a-zA-Z]{2})\}/g, ''), { tld: 'com', to: lang });
-    return await sock.editMessage(msg.key, result[0]);
+    return await msg.reply(result[0]);
   }
 };
