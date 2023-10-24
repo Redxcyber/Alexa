2b4f493beb8cbc6a0239030c69a7b99d53f1eb63
AbhiDev <114732679+AbhiDevOfficial@users.noreply.github.com>
2023-10-24 16:01:40 +0530

    Update yt.js

--- commands/yt.js
+++ commands/yt.js
@@ -4,13 +4,14 @@ module.exports = {
   command: 'yts',
   info: 'Searches video in YouTube for give query.',
   func: async (sock, msg, text) => {
-   if (!text) return await sock.editMessage(msg.key, '*Please enter your query to search in YouTube!*');
-   await sock.editMessage(msg.key, '*🔍 Searching...*');
+   if (!text) return await msg.reply('*Please enter your query to search in YouTube!*');
+   let mesaj = await msg.reply('*🔍 Searching...*');
    try {
     let results = await yt(text);
-    await sock.editMessage(msg.key, results.all.map((v) => '_Title_ : *' + v.title + '*\n_URL_ : ' + (v.url.replace('watch?v=', '').replace('youtube.com', 'youtu.be')) + '\n_Duration_ : *' + v.timestamp + '*\n_Views_ : *' + v.views + '*\n_Uploaded_ : *' + v.ago + '*\n_Uploaded by_ : *' + v.author.name + '*\n_Channel_ : ' + v.author.url).join('\n\n'));
+    await sock.sendMessage(msg.chat, { delete: mesaj.key });
+    await msg.reply(results.all.map((v) => '_Title_ : *' + v.title + '*\n_URL_ : ' + (v.url.replace('watch?v=', '').replace('youtube.com', 'youtu.be')) + '\n_Duration_ : *' + v.timestamp + '*\n_Views_ : *' + v.views + '*\n_Uploaded_ : *' + v.ago + '*\n_Uploaded by_ : *' + v.author.name + '*\n_Channel_ : ' + v.author.url).join('\n\n'));
    } catch {
-    return await sock.editMessage(msg.key, '*❌ Unable to find results for your query!*');
+    return await msg.reply('*❌ Unable to find results for your query!*');
    }
   }
 };
