59802b445bb90ec7b797d7696eb46e0384cb0315
AbhiDev <114732679+AbhiDevOfficial@users.noreply.github.com>
2023-10-24 16:00:05 +0530

    Update video.js

--- commands/video.js
+++ commands/video.js
@@ -5,16 +5,16 @@ module.exports = {
   command: 'video',
   info: 'Downloads video from YouTube from its URL.',
   func: async (sock, msg, text) => {
-    if (!text) return await sock.editMessage('*Please enter a YouTube video url!*');
+    if (!text) return await msg.reply('*Please enter a YouTube video url!*');
     let ytregex = /https:\/\/(?:www\.)?youtube\.com\/watch\?v=[\w-]+|https:\/\/youtu\.be\/[\w-]+|https:\/\/(?:www\.)?youtube\.com\/shorts\/[\w-]+/i;
     let video = '';
     try {
       text = text.match(ytregex)[0] !== undefined ? text.match(ytregex)[0] : (() => { throw false; })()
       video = (text.split('/').slice(-1)[0]).includes('?') ? text.split('/').slice(-1)[0].split('?')[0] : text.split('/').slice(-1)[0];
     } catch {
-      await sock.editMessage(msg.key, '*❌ Invalid url, Please enter a valid youtube video url.*');
+      await msg.reply('*❌ Invalid url, Please enter a valid youtube video url.*');
     }
-    await sock.editMessage(msg.key, '*Downloading video...*');
+    let vim = await msg.reply('*⬇️ Downloading video...*');
     let file = video + '.mp4';
     let vid = ytdl(video, {
       filter: format => format.container === 'mp4' && ['1080p', '720p', '480p', '360p', '240p', '144p']
@@ -23,7 +23,7 @@ module.exports = {
     vid.pipe(fs.createWriteStream(file));
     vid.on('end', async () => {
       await sock.sendMessage(msg.chat,
-       { delete: msg.key }
+       { delete: vim.key }
       ).then(async () => {
        await sock.sendMessage(msg.chat, { video: fs.readFileSync(file), thumbnail: Buffer.alloc(0), mimetype: 'video/mp4' });
        fs.unlinkSync(file);
