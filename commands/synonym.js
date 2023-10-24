ae49f1993cd76f76d9617a2029fa2ed93a3c95fc
AbhiDev <114732679+AbhiDevOfficial@users.noreply.github.com>
2023-10-24 15:55:55 +0530

    Update synonym.js

--- commands/synonym.js
+++ commands/synonym.js
@@ -4,14 +4,14 @@ module.exports = {
   command: 'synonym',
   info: 'Searches synonyms for given term.',
   func: async (sock, msg, text) => {
-    if (!text) return await sock.editMessage(msg.key, '*Please enter a term to find synonyms!*');
+    if (!text) return await msg.reply('*Please enter a term to find synonyms!*');
     let json = await parseJson('https://tuna.thesaurus.com/pageData/' + text);
     let data = json.data.definitionData.definitions[0].synonyms;
-    if (data.length < 1) return await sock.editMessage(msg.key, '*Unable to find synonyms for ' + text + '!*');
+    if (data.length < 1) return await msg.reply('*Unable to find synonyms for ' + text + '!*');
     let res = '*Synonyms for:* _' + text + '_\n\n';
     data.forEach((d, i) => {
-     if (i < 11) res += '*' + (i + 1) + '. ' + d.term + '*\n';
+     if (i <= 10) res += '*' + (i + 1) + '. ' + d.term + '*\n';
     });
-    return await sock.editMessage(msg.key, res);
+    return await msg.reply(res);
   }
 };
