e7d7fab2b83182a5e4304dacaa954e204125a662
AbhiDev <114732679+AbhiDevOfficial@users.noreply.github.com>
2023-10-24 15:44:10 +0530

    Update shorten.js

--- commands/shorten.js
+++ commands/shorten.js
@@ -3,7 +3,7 @@ const { shorten } = require('../helpers/shorten');
 
 module.exports = {
   command: 'shorten',
-  info: 'Shortens given long url using.',
+  info: 'Shortens given long url using cleanuri.',
   func: async (sock, msg, text) => {
     let Text = text !== '' ? text : msg.replied.text ? msg.replied.text : false;
     if (!Text) return await msg.reply('*Please enter or reply to any url to shorten!*');
