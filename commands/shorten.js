5641508d4990a53c4bd8a1d26c8f58ed9ee8dd7d
AbhiDev <114732679+AbhiDevOfficial@users.noreply.github.com>
2023-10-24 15:29:39 +0530

    Create shorten.js

---
+++ commands/shorten.js
@@ -0,0 +1,14 @@
+/*const { isURL } = require('../helpers/utils');
+const { shorten } = require('../helpers/shorten');
+
+module.exports = {
+  command: 'shorten',
+  info: 'Shortens given long url using.',
+  func: async (sock, msg, text) => {
+    let Text = text !== '' ? text : msg.replied.text ? msg.replied.text : false;
+    if (!Text) return await msg.reply('*Please enter or reply to any url to shorten!*');
+    if (!isURL(Text)) return await msg.reply('*❌ Invalid url, Please enter or reply to any valid url only!*');
+    let result = await shorten(Text);
+    return await msg.reply(result);
+  }
+};*/
