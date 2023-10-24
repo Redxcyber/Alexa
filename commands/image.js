75daa439af3182625f979f95e3ee99864521bb3c
AbhiDev <114732679+AbhiDevOfficial@users.noreply.github.com>
2023-10-24 15:51:13 +0530

    Update image.js

--- commands/image.js
+++ commands/image.js
@@ -4,21 +4,21 @@ module.exports = {
   command: 'image',
   info: 'Searches image in google for given query.',
   func: async (sock, msg, text) => {
-    if (!text) return await sock.editMessage(msg.key, '*Please enter your query to search for image in google!*');
-    if (/(porn|xxx|x-rated|adult|sex|nude|erotic|playboy|penthouse|strip|pornographic|liplock|🍑|🍌|🍆|buttock|vagina|dick|penis|pussy)/.test(text)) return await sock.editMessage(msg.key, '*⚠️ This query is banned!*');
+    if (!text) return await msg.reply('*Please enter your query to search for image in google!*');
+    if (/(porn|xxx|x-rated|adult|sex|nude|erotic|playboy|penthouse|strip|pornographic|liplock|🍑|🍌|🍆|buttock|vagina|dick|penis|pussy)/.test(text)) return await msg.reply('*⚠️ This query is banned!*');
     let cl;
     if (cl = text.match('\\{([0-9]{1})\\}')) {
      cl = cl[1]
     } else {
      cl = 5
     }
-    await sock.editMessage(msg.key, '*⬇️ Downloading {} images...*'.replace('{}', cl));
+    await msg.reply('*⬇️ Downloading {} images...*'.replace('{}', cl));
     await g_i_s(text, async (error, result) => {
      let count = []
      count.length = cl
      for (let c of count) {
       let url = result[Math.floor(Math.random() * result.length)].url
-      await sock.sendMessage(msg.chat, { image: { url: url }, mimetype: 'image/png' });
+      await sock.sendMessage(msg.chat, { image: { url: url }, thumbnail: Buffer.alloc(0), mimetype: 'image/png' });
      }
     });
    }
